56a06fe13756656dbb90d9472624b55240a7614e
linki1 <112002373+linki1@users.noreply.github.com>
2023-03-02 21:26:39 +0200

    Action-Wuth-Number
    Action-Wuth-Number

---
+++ l2/script/calk.js
@@ -0,0 +1,10 @@
+//полусаем 2 числа от пользователя 
+let firstNumber = Number(prompt('ведите 1 число'));
+let secondNumber = Number(prompt('ведите 2 число'));
+// выводим деление, умножение, вычитание, сложение. Даных чисел, при помощи alert
+alert(`Користувач ввів ${firstNumber} і ${secondNumber}:
+${firstNumber} + ${secondNumber}=${firstNumber + secondNumber}
+${firstNumber} - ${secondNumber}=${firstNumber - secondNumber}
+${firstNumber} * ${secondNumber}=${firstNumber * secondNumber}
+${firstNumber} / ${secondNumber} =${firstNumber / secondNumber}`
+);
---
+++ l2/script/script.js
@@ -0,0 +1,58 @@
+/*
+let userVariable = prompt();
+let arrayOfNumbers = [];
+function actionsWithVariable(userVariable) {
+
+    let num = 0;
+
+
+    for (let i = 0; i < 10; i++) {
+        switch (userVariable[i]) {
+            case '-':
+                arrayOfNumbers[num] = i;
+                num++;
+                break;
+            case '+':
+                arrayOfNumbers[num] = i;
+                num++;
+                break;
+            case '/':
+                arrayOfNumbers[num] = i;
+                num++;
+                break;
+            case '*':
+                arrayOfNumbers[num] = i;
+                num++;
+                break;
+            default:
+                break;
+        }
+    }
+}
+console.log(actionsWithVariable(userVariable));
+console.log((userVariable.length - ((arrayOfNumbers[0] * 2) + 1)));
+console.log(userVariable.slice((userVariable.length - ((arrayOfNumbers[0] * 2) + 1)), arrayOfNumbers[0]));
+console.log(userVariable.slice(arrayOfNumbers[0] + 1));
+function ctionsWithVariable1() {
+    let sum = 0;
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        switch (userVariable[arrayOfNumbers[i]]) {
+            case '+':
+                let firstnumber = Number(userVariable.slice((userVariable.length - ((arrayOfNumbers[0] * 2) + 1)), arrayOfNumbers[0]));
+                let secondnumber = Number(userVariable.slice(arrayOfNumbers[0] + 1));
+                sum = secondnumber + firstnumber;
+
+                break;
+
+            default:
+                break;
+        }
+
+    }
+    console.log(sum);
+
+}
+console.log(ctionsWithVariable1())*/
+
+
+
